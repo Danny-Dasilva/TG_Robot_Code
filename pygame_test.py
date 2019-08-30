de1cbaa7fc993c02cc3daa2c46c717f11c84dc50
Danny-Dasilva.com <yahchayildasilva@gmail.com>
2019-08-30 01:57:22 -0400

    bugfixing

--- pygame_test.py
+++ pygame_test.py
@@ -11,26 +11,26 @@ pygame.init()
 
 joystick_count = pygame.joystick.get_count()
 print(joystick_count)
-if joystick_count == 0:
-    # No joysticks!
-    print("Error, I didn't find any joysticks.")
-else:
-    # Use joystick #0 and initialize it
-    gamepad = pygame.joystick.Joystick(0)
-    gamepad.init()
+# if joystick_count == 0:
+#     # No joysticks!
+#     print("Error, I didn't find any joysticks.")
+# else:
+#     # Use joystick #0 and initialize it
+#     gamepad = pygame.joystick.Joystick(0)
+#     gamepad.init()
 
 
-while True:
+# while True:
 
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
+#     for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 done = True
          
-            if joystick_count != 0:
+#             if joystick_count != 0:
 
 
-                val = gamepad.get_button(0)
-                val_2 = gamepad.get_button(1)
+#                 val = gamepad.get_button(0)
+#                 val_2 = gamepad.get_button(1)
                 
 
 
@@ -38,22 +38,22 @@ while True:
 
 
                 
-            else:
-                print('no joysticks')
+#             else:
+#                 print('no joysticks')
             
 
 
 
 
 
-            #  Arm motor
-            if val == 1:
+#             #  Arm motor
+#             if val == 1:
                 
-                print("armA_forward active")
-            elif val_2 == 1:
-                print("armA_back")
-            else:
-                print('no val')
+#                 print("armA_forward active")
+#             elif val_2 == 1:
+#                 print("armA_back")
+#             else:
+#                 print('no val')
             
 
 
