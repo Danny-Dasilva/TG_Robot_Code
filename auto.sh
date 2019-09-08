378cf6aa4b5743780897b09bd56f77a7a256de8a
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-09-08 00:59:31 +0000

    fixed raspi autoboot

--- auto.sh
+++ auto.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-line="@reboot sudo python3 /home/mendel/TG-Coral-Robot_Code/RobotCode.py"
-(crontab -u "mendel" -l; echo "$line" ) | crontab -u "mendel" -
 
+if grep -s -q "MX8MQ" /sys/firmware/devicetree/base/model; then
+	echo "installing auto code on Coral"
+  line="@reboot sudo python3 /home/mendel/TG-Coral-Robot_Code/RobotCode.py"
+(crontab -u "pi" -l; echo "$line" ) | crontab -u "pi" -
+
+
+else
+  # Install gstreamer
+  #sudo apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-good py$
+
+  if grep -s -q "Raspberry Pi" /sys/firmware/devicetree/base/model; then
+	echo "installing auto code on pi"
+    line="@reboot sudo python3 /home/pi/Desktop/TG-Coral-Robot_Code/RobotCode.py"
+		(crontab -u "pi" -l; echo "$line" ) | crontab -u "pi" -
+
+  
+
+
+  fi
+fi
--- install.sh
+++ install.sh
@@ -10,16 +10,21 @@ if grep -s -q "MX8MQ" /sys/firmware/devicetree/base/model; then
 
 else
   # Install gstreamer
-  sudo apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-good py$
+  #sudo apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-good py$
 
   if grep -s -q "Raspberry Pi" /sys/firmware/devicetree/base/model; then
     echo "Installing Raspberry Pi specific dependencies"
-    sudo apt-get install python3-rpi.gpio
-    # Add v4l2 video module to kernel
-    if ! grep -q "bcm2835-v4l2" /etc/modules; then
-      echo bcm2835-v4l2 | sudo tee -a /etc/modules
-    fi
-    sudo modprobe bcm2835-v4l2
+    
+  echo "Installing Servokit"
+  sudo pip3 install adafruit-circuitpython-servokit
+  echo "Adding Libgiod Bindings"
+  wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/libgpiod.sh
+  chmod +x libgpiod.sh
+  ./libgpiod.sh
+  rm -rf ./libgpiod.sh
+	sudo pip3 install pygame
+
+
   fi
 fi
 
