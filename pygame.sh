2e3bec34d152c6f93c8053a1920ccdfa8df672f1
Danny Dasilva <yahchayildasilva@gmail.com>
2019-09-27 14:32:30 -0400

    Update pygame.sh

--- pygame.sh
+++ pygame.sh
@@ -8,7 +8,7 @@ if grep -s -q "MX8MQ" /sys/firmware/devicetree/base/model; then
   sudo apt-get install python3-dev python3-numpy libsdl-dev libsdl-image1.2-dev \
   libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libportmidi-dev \
   libavformat-dev libswscale-dev libjpeg-dev libfreetype6-dev -y
-  python3 setup.py build
+  sudo python3 setup.py build
   sudo python3 setup.py install
   cd ..
   sudo rm -rf pygame
