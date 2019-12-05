1f058b8222f38a48e2bd8edd80c8e032c4313d6e
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-12-05 22:03:48 +0000

    workign on audio bugs

--- Audio/audio_loop.py
+++ Audio/audio_loop.py
@@ -1,35 +1,42 @@
 
-# Import the required module for text  
-# to speech conversion 
+#!/usr/bin/python3
 from gtts import gTTS 
 import socket    
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-IP = s.getsockname()[0]
-s.close()
-hostname = socket.gethostname() 
-# This module is imported so that we can  
-# play the converted audio 
-import os 
+import os
+from time import sleep
+sleep(3)
+path = os.path.dirname(os.path.abspath(__file__))
+
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.connect(("8.8.8.8", 80))
+# IP = s.getsockname()[0]
+# s.close()
+# hostname = socket.gethostname() 
+
   
 
-# Language in which you want to convert 
-language = 'en'
+# # Language in which you want to convert 
+# language = 'en'
   
-# Passing the text and language to the engine,  
-# here we have marked slow=False. Which tells  
-# the module that the converted audio should  
-# have a high speed 
-myhost = gTTS(text=hostname, lang=language, slow=False) 
-myIP = gTTS(text=IP, lang=language, slow=False) 
-# Saving the converted audio in a mp3 file named 
-# welcome  
-myhost.save("host.mp3") 
-myIP.save("ip.mp3") 
-# Playing the converted file 
+# # Passing the text and language to the engine,  
+# # here we have marked slow=False. Which tells  
+# # the module that the converted audio should  
+# # have a high speed 
+# myhost = gTTS(text=hostname, lang=language, slow=False) 
+# myIP = gTTS(text=IP, lang=language, slow=False) 
+# status = gTTS(text="none", lang=language, slow=False) 
+
+# # Saving the converted audio in a mp3 file named 
+# # welcome  
+# myhost.save(path + "/host.mp3") 
+# myIP.save( path + "/ip.mp3") 
+# # status.save( path +  "/status.mp3")
+
+# # Playing the converted file 
 while True:
-
-    os.system("mplayer host.mp3") 
-    os.system("mplayer ip.mp3") 
-    os.system("mplayer status.mp3") 
+    print(path)
+    os.system("mplayer " + path +  "/host.mp3") 
+    os.system("mplayer " + path +  "/ip.mp3") 
+    os.system("mplayer " + path +   "/status.mp3") 
 
--- app/Audio.py
+++ app/Audio.py
@@ -1,5 +1,5 @@
 from gtts import gTTS 
-
+import os
 
 class Audio():
     def __init__(self):
@@ -10,6 +10,6 @@ class Audio():
             language = 'en'
             
             status = gTTS(text=string, lang=language, slow=False) 
-            status.save("Audio/status.mp3")
+            status.save("/home/mendel/TG_Robot_Code/Audio/status.mp3")
         
         self.previous_string = string
