4e326e2b233721cfeb290f1cad3c9e24ceb6c465
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-12-05 21:04:33 +0000

    added update for player

---
+++ scripts/update.sh
@@ -0,0 +1,28 @@
+
+if grep -s -q "MX8MQ" /sys/firmware/devicetree/base/model; then
+  echo "audio backround"
+  pip3 install pip install gTTS
+  sudo apt-get install mplayer
+  sudo apt-get install alsa-utils
+
+  sudo amixer -D pulse sset Master 50%
+
+
+  
+
+else
+  # Install gstreamer
+  sudo apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-good py$
+
+  if grep -s -q "Raspberry Pi" /sys/firmware/devicetree/base/model; then
+      echo "audio backround"
+      pip3 install pip install gTTS
+      sudo apt-get install mplayer
+      sudo apt-get install alsa-utils
+
+      amixer -D pulse sset Master 50%
+  fi
+fi
+
+
+
