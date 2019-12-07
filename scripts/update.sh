3ec5c9d3683519d2ef9c2d4f75b86c5105f00cf2
Danny Dasilva <yahchayildasilva@gmail.com>
2019-12-07 13:53:14 -0500

    update pi config

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,16 +11,13 @@ if grep -s -q "MX8MQ" /sys/firmware/devicetree/base/model; then
   
 
 else
-  # Install gstreamer
-  sudo apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-good py$
-
   if grep -s -q "Raspberry Pi" /sys/firmware/devicetree/base/model; then
       echo "audio backround"
-      pip3 install pip install gTTS
+      sudo pip3 install gTTS
       sudo apt-get install mplayer
       sudo apt-get install alsa-utils
 
-      amixer -D pulse sset Master 50%
+      amixer sset 'Master' 50%
   fi
 fi
 
