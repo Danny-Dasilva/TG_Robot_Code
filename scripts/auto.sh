6acc3b01927638ce6dc9b0c6877e624d4d31c744
Danny Dasilva <yahchayildasilva@gmail.com>
2019-12-08 18:08:45 -0500

    updated crontab

--- scripts/auto.sh
+++ scripts/auto.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
-
+export ROUTE=$PWD
 if grep -s -q "MX8MQ" /sys/firmware/devicetree/base/model; then
 	echo "installing auto code on Coral"
-  line="@reboot sudo python3 /home/mendel/TG_Robot_Code/RobotCode.py"
-(crontab -u "mendel" -l; echo "$line" ) | crontab -u "mendel" -
-
+  crontab -r
+  line="@reboot python3 $ROUTE/Audio/audio_loop.py"
+  (crontab -u "mendel" -l; echo "$line" ) | crontab -u "mendel" -
+  line="@reboot $ROUTE/Audio/test.sh "
+  (crontab -u "mendel" -l; echo "$line" ) | crontab -u "mendel" -
+  line="@reboot sudo python3 $ROUTE/RobotCode.py"
+  (crontab -u "mendel" -l; echo "$line" ) | crontab -u "mendel" -
 
 else
   # Install gstreamer
@@ -13,11 +16,10 @@ else
 
   if grep -s -q "Raspberry Pi" /sys/firmware/devicetree/base/model; then
 	echo "installing auto code on pi"
-    line="@reboot sudo python3 /home/pi/Desktop/TG_Robot_Code/RobotCode.py"
-		(crontab -u "pi" -l; echo "$line" ) | crontab -u "pi" -
-
   
-
-
-  fi
-fi
+	line="@reboot python3 $ROUTE/Audio/audio_loop.py"
+  (crontab -u "pi" -l; echo "$line" ) | crontab -u "pi" -
+  line="@reboot $ROUTE/Audio/test.sh "
+  (crontab -u "pi" -l; echo "$line" ) | crontab -u "pi" -
+  line="@reboot sudo python3 $ROUTE/RobotCode.py"
+  (crontab -u "pi" -l; echo "$line" ) | crontab -u "pi" 
